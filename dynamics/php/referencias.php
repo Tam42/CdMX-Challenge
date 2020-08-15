20613db9977644f2a7ef38a97505d106d9f70574
LALECHERA <regina-blue@hotmail.com>
2020-08-14 21:10:05 -0500

    referencias php
    pagina de referencias

---
+++ dynamics/php/referencias.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CORONABOT | Referencias</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../statics/CSS/style.css">
+</head>
+
+<body>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-10">
+                <h4 class="text-uppercase text-center">Estas son los artículos y websites de los que se ha obtenido la
+                    información para el CoronaBot</h1>
+            </div>
+            <div class="col-1"></div>
+        </div>
+        <br><br>
+        <div class="row">
+            <div class="col-sm text-center"></div>
+            <ul>
+                <li>Carfì A, Bernabei R, Landi F, for the Gemelli Against COVID-19 Post-Acute Care Study Group. Persistent
+                    Symptoms in Patients After Acute COVID-19. JAMA. 2020;324(6):603–605. doi:10.1001/jama.2020.12603</li><br>
+                <li>Li, G, Fan, Y, Lai, Y, et al. Coronavirus infections and immune responses. J Med Virol. 2020; 92:
+                    424– 432. https://doi.org/10.1002/jmv.25685</li><br>
+                <li>Wu LP. Wang NCh, Chang Yh et al. Duration of Antibody Responses after Severe Acute Respiratory
+                    Syndrome. Emerg Infect Dis. 2007 Oct; 13(10): 1562–1564.</li><br>
+                <li>Bao L, Deng W, Gao H, Xiao Ch, Liu J, Xue J, et al. Reinfection could not occur in SARS-CoV-2
+                    infected rhesus macaques. BioRxiv. 2020. https://doi.org/10.1101/2020.03.13.990226</li><br>
+                <li>Y, Mo P, Xiao Y, Zhao O, Zhang Y, Wang F. Post-discharge surveillance and positive virus detection
+                    in two medical staff recovered from coronavirus disease 2019 (COVID-19), China, January to February
+                    2020. Eurosurveillance, 2020; 25</li><br>
+                <li>Organización Mundial de la Salud: <a href="https://www.who.int/es/health-topics/coronavirus">link</a> </li> 
+            </ul>
+            
+            <div class="col-sm"></div>
+        </div>
+    </div>
+
+    <br>
+    <br>
+    <br>
+    <footer class="footer mt-auto py-3">
+        Chatbot desarrollado en Challenge CdeCMx 2020 con Thomas Donoghue.
+    </footer>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
